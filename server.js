4787e654576aac8e9651eab732ef57e49f4f931c
Srinugopi09 <srinigopishetty@gmail.com>
2016-05-04 07:37:55 -0400

    Add Post for todos

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Going Gorcery Shopping',
-	isCompleted: false
-},
-{
-	id: 2,
-	description:'Meet Mom for Lunch',
-	isCompleted:true,
-},
-{
-	id: 3,
-	description: 'Read a Book',
-	isCompleted: false
-}];
+var bodyParser = require('body-parser');
+
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Todo API Root');
 });
 
 //GET /todos
-
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
-//GET /todos/:id
 
+//GET /todos/:id
 app.get('/todos/:id', function (req, res){
 
 	var todoId = parseInt(req.params.id, 10) ;
@@ -43,8 +33,6 @@ app.get('/todos/:id', function (req, res){
 			mactchedTodo = todo;
 		}
 	});
-
-	
 	if (mactchedTodo) {
 		res.json(mactchedTodo);
 	} else {
@@ -52,11 +40,21 @@ app.get('/todos/:id', function (req, res){
 		console.log(mactchedTodo);
 		res.status(404).send();
 	}
-
-
-	//res.send('Asking for todo with id of ' + req.params.id);
-
+//res.send('Asking for todo with id of ' + req.params.id);
 });
+
+//POST /todos/:id
+app.post('/todos', function (req,res) {
+
+	var body = req.body;
+	body.id = todoNextId;
+	todoNextId += todoNextId;
+
+	todos.push(body);
+
+	res.send(body);
+
+})
 
 
 
